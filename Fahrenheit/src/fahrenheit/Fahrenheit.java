04529ee6b03e6db2f1d4f42a379345d6c21d5c70
Ivan Day <ivanday9@gmail.com>
2015-11-11 11:04:36 -0600

    I think this will work

--- Fahrenheit/src/fahrenheit/Fahrenheit.java
+++ Fahrenheit/src/fahrenheit/Fahrenheit.java
@@ -15,7 +15,8 @@ public class Fahrenheit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        FahrenheitGUI converter = new FahrenheitGUI();
+        converter.display();
     }
     
 }
